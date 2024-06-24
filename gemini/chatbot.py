57370b4686e27004c783e2b644cc70bc77686456
Pranav <pranavmurali024@gmail.com>
2024-06-25 01:18:07 +0530

    final code

---
+++ gemini/chatbot.py
@@ -0,0 +1,18 @@
+import os
+import google.generativeai as genai
+
+
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key="GOOGLE_API_KEY")
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+while True:
+    prompt = input("Ask me anything: ")
+    if (prompt == "exit"):
+        break
+    response = chat.send_message(prompt, stream=True)
+    for chunk in response:
+        if chunk.text:
+          print(chunk.text)
